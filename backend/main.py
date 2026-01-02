bbb87f150d0e242986cdcdb8a8f589d01c7b23dd
MOID UDDIN <moiduddin990@gmail.com>
2026-01-02 18:23:26 +0500

    second commit

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,8 @@ app = FastAPI(title="Todo App (FastAPI + Neon + JWT + Gemini)")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-        "http://127.0.0.1:3000",],
+        "http://127.0.0.1:3000",
+        "https://hackathon-todo-gamma.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
